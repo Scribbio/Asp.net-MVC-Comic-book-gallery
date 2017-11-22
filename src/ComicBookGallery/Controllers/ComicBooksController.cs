887f9cc344ffe458bbb10888d65de1c1809b5e16
Scribbio <joeditrolio@outlook.com>
2017-11-22 16:57:14 +0000

    Added List view.

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,31 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            
+        private ComicBookRepository _comicBookRepository = null;
 
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        //Action methods render view pages.
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.getComicBook((int)id);
+
+            //Creates a ViewResult object that renders a view to the response.
             return View(comicBook);
 
         }
--- src/ComicBookGallery/Data/comicBookRepository.cs
+++ src/ComicBookGallery/Data/comicBookRepository.cs
@@ -4,9 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
+
+/**
+ * We can think of this class as a central location, or repository, for storing and
+ * managing our comic book model instances. Let's name this class ComicBook Repository. 
+ * By organizing our data access code in this way, we're implementing the Repository Design Pattern. 
+ * Creating a design where we don't repeat ourselves allows for our classes to naturally be more tightly focused. 
+ * Each class should have a single responsibility, or concern, and not overlap with any other classes on our website. 
+ * This is known as the separation of concerns design principle. Separating concerns also helps make our 
+ * website easier to maintain. With our improved design, if we wanted how our comic book model instances 
+ * are created or retrieved, we'd only need to change the comic book repository class.
+ * 
+
+ */
+
 namespace ComicBookGallery.Data
 {
-    public class comicBookRepository
+    public class ComicBookRepository
     {
         //By making this field static, we're associating it with the ComicBook -
         //repository class instead of the individual object instances of this class. 
@@ -14,6 +29,7 @@ namespace ComicBookGallery.Data
         {
             new ComicBook()
             {
+                Id = 1,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
@@ -29,6 +45,7 @@ namespace ComicBookGallery.Data
             },
             new ComicBook()
             {
+                Id = 2,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
                 DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -44,6 +61,7 @@ namespace ComicBookGallery.Data
             },
             new ComicBook()
             {
+                Id = 3,
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
                 DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
@@ -57,6 +75,11 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
         };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
 
         public ComicBook getComicBook(int id)
         {
